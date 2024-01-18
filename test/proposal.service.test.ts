6392652e5f64fc04f0d7e087cb558729a36b19a3
Mona Lisa <monalisa@davinci.com>
2024-01-18 17:47:36 +0700

    fix: jest module name mapper

--- test/proposal.service.test.ts
+++ test/proposal.service.test.ts
@@ -1,12 +1,29 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import database from '~/config/database.config';
+import mail from '~/config/mail.config';
+import redis from '~/config/redis.config';
+import token from '~/config/token.config';
+import { DatabaseModule } from '~/database/typeorm';
 import { CreateProposalDto } from '~/modules/proposal/dto/create-proposal.dto';
 import { ProposalService } from '~/modules/proposal/proposal.service';
+import { SharedModule } from '~/shared/shared.module';
 
 describe('ProposalService', () => {
     let service: ProposalService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                DatabaseModule,
+                SharedModule,
+                ConfigModule.forRoot({
+                    isGlobal: true,
+                    envFilePath: ['.env'],
+                    load: [token, database, mail, redis],
+                    cache: true,
+                }),
+            ],
             providers: [ProposalService],
         }).compile();
 
@@ -17,6 +34,7 @@ describe('ProposalService', () => {
         it('should create a new proposal', () => {
             const createProposalDto: CreateProposalDto = {
                 typeId: 1,
+                warehouseId: 1,
                 name: 'Test',
                 content: 'Test',
                 details: [
