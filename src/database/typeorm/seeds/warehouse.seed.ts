421f90ffe1b38cf48efc88b0c4558fb33a47f80c
Salvator Mundi <monalisa@davinci.com>
2024-02-19 22:50:53 +0700

    fix: remove warehouse type

--- src/database/typeorm/seeds/warehouse.seed.ts
+++ src/database/typeorm/seeds/warehouse.seed.ts
@@ -1,13 +1,9 @@
 import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { WarehouseEntity } from '~/database/typeorm/entities/warehouse.entity';
-import { WarehouseTypeEntity } from '~/database/typeorm/entities/warehouseType.entity';
 
 export default class WarehouseSeeder implements Seeder {
     public async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<any> {
-        const warehouseTypeFactory = factoryManager.get(WarehouseTypeEntity);
-        await warehouseTypeFactory.saveMany(5);
-
         const warehouseFactory = factoryManager.get(WarehouseEntity);
         await warehouseFactory.saveMany(5);
     }
--- src/modules/warehouse-type/dto/update-warehouse-type.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateWarehouseTypeDto } from '~/modules/warehouse-type/dto/create-warehouse-type.dto';
-
-export class UpdateWarehouseTypeDto extends PartialType(CreateWarehouseTypeDto) {}
